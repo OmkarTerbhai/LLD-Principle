32cf533218585d7262698e3aeca240cb58ef3300
OmkarTerbhai <omkarterbhai@gmail.com>
2024-01-11 21:21:17 +0530

    Product class prototype.

---
+++ src/Practice1/Product.java
@@ -0,0 +1,13 @@
+package Practice1;
+
+public abstract class Product {
+    private String name;
+    private int cost;
+    private String type;
+
+    private String category;
+
+    private int discount;
+
+    private boolean isEMIPurchasable;
+}
